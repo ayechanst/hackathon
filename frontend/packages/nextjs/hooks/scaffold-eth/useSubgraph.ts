d87a7c57df8de5b23c6b86fed9b436c3ca9be1d2
John Corrigan <jcorrigan322@gmail.com>
2024-02-25 21:53:43 -0700

    useSubgraph hook

---
+++ frontend/packages/nextjs/hooks/scaffold-eth/useSubgraph.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+type UseSubgraphProps = {
+  rows: number;
+  orderDirection: "asc" | "desc";
+};
+
+export function useSubgraph({ rows, orderDirection }: UseSubgraphProps) {
+  const endpoint = "https://api.studio.thegraph.com/query/64372/test-genesis/0.0.2";
+  const [data, setData] = useState<any>(null);
+
+  const queryAddys = `
+    query {
+        addresses(first: ${rows}, orderBy: numTxs, orderDirection: ${orderDirection}) {
+            id
+            numTxs
+            isContract
+        }
+    }`;
+
+  const fetchSubgraph = async (query: String) => {
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    });
+
+    const { data } = await response.json();
+    setData(data.addresses);
+  };
+
+  useEffect(() => {
+    fetchSubgraph(queryAddys).then(data => console.log(data));
+  }, []);
+
+  return { data };
+}
