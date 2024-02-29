1b5b8e7e0fcb700939cd7d6bb0e6baa32507e1ed
John Corrigan <jcorrigan322@gmail.com>
2024-02-28 23:10:44 -0700

    user token holders and stuff

--- substreams/src/erc20maps.rs
+++ substreams/src/erc20maps.rs
@@ -1,9 +1,12 @@
-use crate::pb::debbie::{Erc20Transfer, Erc20Transfers, MasterProto};
+use crate::pb::debbie::{Erc20Transfer, Erc20Transfers, MasterProto, TokenHolder, TokenHolders};
 use substreams::scalar::BigInt;
-use substreams::store::{StoreGet, StoreGetBigInt};
+use substreams::store::{StoreGet, StoreGetBigInt, StoreGetInt64};
 
 #[substreams::handlers::map]
-pub fn map_erc20_transfer_vol(transfers: MasterProto,store: StoreGetBigInt) -> Result<Erc20Transfers, substreams::errors::Error> {
+pub fn map_erc20_transfer_vol(
+    transfers: MasterProto,
+    store: StoreGetBigInt,
+) -> Result<Erc20Transfers, substreams::errors::Error> {
     let mut erc20_transfers = Vec::new();
     for mut transfer in transfers.erc20transfers {
         if let Some(volume_bigint) = store.get_at(0, &transfer.address) {
@@ -11,5 +14,57 @@ pub fn map_erc20_transfer_vol(transfers: MasterProto,store: StoreGetBigInt) -> R
             erc20_transfers.push(transfer);
         }
     }
-    Ok(Erc20Transfers {transfers: erc20_transfers})
+    Ok(Erc20Transfers {
+        transfers: erc20_transfers,
+    })
+}
+
+#[substreams::handlers::map]
+pub fn map_erc20_count(
+    transfers: MasterProto,
+    store: StoreGetInt64,
+) -> Result<Erc20Transfers, substreams::errors::Error> {
+    let mut erc20_transfers = Vec::new();
+    for mut transfer in transfers.erc20transfers {
+        if let Some(count) = store.get_at(0, &transfer.address) {
+            transfer.count = count.to_string();
+        }
+        erc20_transfers.push(transfer);
+    }
+    Ok(Erc20Transfers {
+        transfers: erc20_transfers,
+    })
+}
+
+#[substreams::handlers::map]
+pub fn map_erc20_token_holders(
+    transfers: MasterProto,
+    store: StoreGetInt64,
+) -> Result<TokenHolders, substreams::errors::Error> {
+    let mut token_holders = Vec::new();
+    for transfer in &transfers.erc20transfers {
+        if transfer.to != "00000000000000000000".to_string() {
+            token_holders.push(TokenHolder {
+                holder_address: transfer.to.to_string(),
+                token_address: transfer.address.to_string(),
+                balance: String::new(),
+                transfer_volume: String::new(),
+                transfer_count: String::new(),
+                transfer_amount: transfer.amount.to_string(),
+                transfer_from: false,
+            });
+        }
+        token_holders.push(TokenHolder {
+            holder_address: transfer.from.to_string(),
+            token_address: transfer.address.to_string(),
+            balance: String::new(),
+            transfer_volume: String::new(),
+            transfer_count: String::new(),
+            transfer_amount: transfer.amount.to_string(),
+            transfer_from: true,
+        });
+    }
+    Ok(TokenHolders {
+        token_holders: token_holders,
+    })
 }
--- substreams/src/erc20stores.rs
+++ substreams/src/erc20stores.rs
@@ -1,18 +1,57 @@
 use std::str::FromStr;
 
-use crate::pb::debbie::{Erc20Deployment, Erc20Transfer, MasterProto};
+use crate::pb::debbie::{Erc20Deployment, Erc20Transfer, MasterProto, TokenHolders};
 use crate::pb::debbie::{Erc721Deployment, Erc721Transfer};
 use substreams::scalar::BigInt;
-use substreams::store::{StoreAdd, StoreAddBigInt, StoreNew};
+use substreams::store::{StoreAdd, StoreAddBigInt, StoreAddInt64, StoreNew};
 use substreams::Hex;
 
 #[substreams::handlers::store]
 pub fn store_erc20_transfer_vol(transfers: MasterProto, store: StoreAddBigInt) {
     for transfer in transfers.erc20transfers {
+        //add check for these
         store.add(
             0,
             transfer.address,
             BigInt::from_str(&transfer.amount).unwrap(),
         )
+    }
+}
+
+#[substreams::handlers::store]
+pub fn store_erc20_count(transfers: MasterProto, store: StoreAddInt64) {
+    for transfer in transfers.erc20transfers {
+        store.add(0, transfer.address, 1)
+    }
+}
+
+#[substreams::handlers::store]
+pub fn store_user_erc20_vol(transfers: TokenHolders, store: StoreAddBigInt) {
+    for transfer in transfers.token_holders {
+        //add check for these
+        store.add(
+            0,
+            format!("{}:{}", transfer.holder_address, transfer.token_address),
+            BigInt::from_str(&transfer.transfer_amount).unwrap(),
+        );
+    }
+}
+
+#[substreams::handlers::store]
+pub fn store_user_erc20_balance(transfer: TokenHolders, store: StoreAddBigInt) {
+    for transfer in transfer.token_holders {
+        //add if let some check??
+        let amount: substreams::scalar::BigInt;
+        if !transfer.transfer_from {
+            amount = BigInt::from_str(&transfer.transfer_amount).unwrap();
+        } else {
+            amount = BigInt::from_str(&transfer.transfer_amount).unwrap() * -1;
+        }
+
+        store.add(
+            0,
+            format!("{}:{}", transfer.holder_address, transfer.token_address),
+            amount,
+        );
     }
 }
