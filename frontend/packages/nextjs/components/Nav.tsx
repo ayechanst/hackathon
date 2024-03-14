dfe2c5b857d3e0ae6fe541bc4f60d2cbdb55b422
ayechan <ayechansant@gmail.com>
2024-03-14 02:51:29 -0700

    final deployment no cap

--- frontend/packages/nextjs/components/Nav.tsx
+++ frontend/packages/nextjs/components/Nav.tsx
@@ -8,7 +8,7 @@ const Nav = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setIsPulsating(prevState => !prevState);
-    }, 500); // 1000 milliseconds = 1 seconds
+    }, 500);
     return () => clearInterval(interval);
   }, []);
 
